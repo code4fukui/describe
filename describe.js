a3409d3fc108a9ffcd444f4ad27bc27200aedce3
Taisuke Fukuno <fukuno@jig.jp>
2023-12-23 13:01:35 +0900

    add funcs

--- describe.js
+++ describe.js
@@ -1,6 +1,13 @@
 import * as t from "https://deno.land/std/testing/asserts.ts";
 
-export const assert = t;
+export const assert = t.assert;
+assert.assert = t.assert;
+assert.equal = t.assertEquals;
+assert.doesNotThrow = f => f();
+assert.throws = t.assertThrows;
+assert.deepEqual = t.assertEquals;
+
+//console.log(t);
 
 export const describe = (name, func) => func();
 export const it = (name, func) => Deno.test(name, func);
